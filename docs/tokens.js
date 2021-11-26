74f562229da21c0755efbd676989d24853351f18
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2021-11-26 14:27:39 +1100

    Tidy

--- docs/tokens.js
+++ docs/tokens.js
@@ -88,9 +88,12 @@ const Tokens = {
                       <b-card>
                         <font size="-2">
                           <b-table small fixed striped sticky-header="1000px" :fields="scanOwners.fields" :items="scanOwners.owners" head-variant="light">
+                            <template #cell(owner)="data">
+                              <b-link :href="explorer + 'address/' + data.item.owner" class="card-link truncate" target="_blank">{{ data.item.owner }}</b-link>
+                            </template>
                             <template #cell(tokenURI)="data">
                               <div v-if="scanOwners.tokenURIs[data.item.tokenId]">
-                                <b-link :href="scanOwners.tokenURIs[data.item.tokenId].replace('ipfs://', 'https://ipfs.io/ipfs/')" class="card-link" target="_blank">{{ scanOwners.tokenURIs[data.item.tokenId] }}</b-link>
+                                <b-link :href="scanOwners.tokenURIs[data.item.tokenId].replace('ipfs://', 'https://ipfs.io/ipfs/')" class="card-link truncate" target="_blank">{{ scanOwners.tokenURIs[data.item.tokenId] }}</b-link>
                               </div>
                             </template>
                             <template #cell(image)="data">
