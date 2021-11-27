a2d25e0cce503b30bb918c6411537d04af0800fa
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2021-11-28 09:21:45 +1100

    Collections WIP

--- docs/collectionDataModule.js
+++ docs/collectionDataModule.js
@@ -307,6 +307,7 @@ const collectionDataModule = {
                   topics: [[
                     '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef', // Transfer(index_topic_1 address from, index_topic_2 address to, index_topic_3 uint256 tokenId)
                     '0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31', // ApprovalForAll (index_topic_1 address owner, index_topic_2 address operator, bool approved)
+                    // '0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925', // Approval (index_topic_1 address src, index_topic_2 address guy, uint256 wad)
                   ]],
                 };
                 const events = await provider.getLogs(filter);
